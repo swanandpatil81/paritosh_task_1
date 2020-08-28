f2e588027d750db24d2d84032857744e7b505409
Paritosh Mondkar <paritoshmondkar@gmail.com>
2020-08-28 18:53:31 +0530

    changes made

--- TestProject/app/Http/Controllers/HomeController.php
+++ TestProject/app/Http/Controllers/HomeController.php
@@ -80,8 +80,7 @@ class HomeController extends Controller{
 
     	$smooth_road_seconds = ($smooth_road_distance / $SMOOTH_ROAD) * 60 * 60;
     	
-    	echo "Smooth Road Journey Will take ".$this -> secondsToTime($smooth_road_seconds);
-    	echo "<br>";
+        $this -> pretty_print("Smooth Road Journey Will take ".$this -> secondsToTime($smooth_road_seconds));
 
     	return $smooth_road_seconds;
 
@@ -90,9 +89,8 @@ class HomeController extends Controller{
     public function display_bad_road_data($badroad_distance , $BAD_ROAD){
 
     	$bad_road_seconds = ($badroad_distance / $BAD_ROAD) * 60 * 60;
-    	
-    	echo "Bad Road Journey Will take ".$this -> secondsToTime($bad_road_seconds);
-    	echo "<br>";
+
+        $this -> pretty_print("Bad Road Journey Will take ".$this -> secondsToTime($bad_road_seconds));
 
     	return $bad_road_seconds;
 
@@ -101,9 +99,8 @@ class HomeController extends Controller{
     public function display_wip_road_data($workinprogress_distance , $WIP_ROAD){
 
     	$wip_road_seconds = ($workinprogress_distance / $WIP_ROAD) * 60 * 60;
-    	
-    	echo "WIP Road Journey Will take ".$this -> secondsToTime($wip_road_seconds);
-    	echo "<br>";
+
+        $this -> pretty_print("WIP Road Journey Will take ".$this -> secondsToTime($wip_road_seconds));
 
     	return $wip_road_seconds;
 
@@ -112,24 +109,27 @@ class HomeController extends Controller{
     public function display_break_data($breaks_taken){
 
     	$breaks_seconds = $breaks_taken * 60;
-    	
-    	echo "Breaks during Journey Will take ".$this -> secondsToTime($breaks_seconds);
-    	echo "<br>";
+
+        $this -> pretty_print("Breaks during Journey Will take ".$this -> secondsToTime($breaks_seconds));
 
     	return $breaks_seconds;
 
     }
 
     public function display_total_journey_data($total_journey_seconds){
-    	
-    	echo "Total Journey Will take ".$this -> secondsToTime($total_journey_seconds);
-    	echo "<br>";
+
+        $this -> pretty_print("Total Journey Will take ".$this -> secondsToTime($total_journey_seconds));
 
     }
 
-    function secondsToTime($seconds) {
+    public function secondsToTime($seconds) {
 	    $dtF = new \DateTime('@0');
 	    $dtT = new \DateTime("@$seconds");
 	    return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
 	}
+
+    public function pretty_print($statement){
+        echo $statement;
+        echo "<br>";
+    }
 }
